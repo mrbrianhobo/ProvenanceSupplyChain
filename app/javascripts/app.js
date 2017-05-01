09472837d983820a183ab5169d86a05ed51df05c
Raymond Chu <raychu@berkeley.edu>
2017-05-01 13:51:55 -0700

    final demo

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -17,41 +17,50 @@ const EthereumClient = new Web3(new Web3.providers.HttpProvider("http://localhos
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
+var myAccounts = EthClient.eth.accounts;
+var myAccount = myAccounts[0];
 var accounts;
-var account;
+var account = myAccounts[0]; 
 
 window.App = {
   start: function() {
     var self = this;
     // SupplyChain.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
-    SupplyChain.setProvider(web3.currentProvider);
-    console.log("Provider set.");
+    // SupplyChain.setProvider(web3.currentProvider);
+    // console.log("Provider set.");
 
     // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
+    // web3.eth.getAccounts(function(err, accs) {
+    //   if (err != null) {
+    //     alert("There was an error fetching your accounts.");
+    //     return;
+    //   }
 
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
+    //   if (accs.length == 0) {
+    //     alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+    //     return;
+    //   }
 
-      accounts = accs;
-      account = accounts[0];
+    //   accounts = accs;
+    //   account = myAccounts[0];
+         account = myAccounts[0];
+    // });
 
-    });
+  },
+
+  swapAccount: function(number) {
+    var self = this;
+    account = myAccounts[number];
+    console.log("You have swapped to:" + account);
   },
 
   joinContract: function(name) {
     var self = this;
-      var x = smartContract.join.sendTransaction(name,  {from: EthClient.eth.accounts[0], gas:1000000});
+      var x = smartContract.join.sendTransaction(name,  {from: account, gas:1000000});
       console.log(x);
 
-      if(smartContract.hasJoined.call()){
-        var currName = smartContract.getYourName.call();
+      if(smartContract.hasJoined.call({from: account})){
+        var currName = smartContract.getYourName.call({from: account});
         if (currName == name) {
             return "You have joined the contract as " + currName;
         }
@@ -66,123 +75,129 @@ window.App = {
 
   viewFunds: function() {
       var self = this;
-      var funds = smartContract.viewFunds.call().toNumber();
+      var funds = smartContract.viewFunds.call({from: account}).toNumber();
       return funds;
+  },
+
+  getCurrentAddress: function() {
+    var self = this;
+    return account;
   },
 
   getMyName: function() {
       var self = this;
-      var name = smartContract.getYourName.call();
+      var name = smartContract.getYourName.call({from: account});
       return name;
   },
 
   getOwnerName: function(addr){
     var self = this;
-    var name = smartContract.getOwnerName.call(addr);
+    var name = smartContract.getOwnerName.call(addr, {from: account});
     return name;
   },
 
   getCurrentOwner: function(serial) {
       var self = this;
-      var owner = smartContract.getCurrentOwner.call(serial);
+      var owner = smartContract.getCurrentOwner.call(serial, {from: account});
       return owner;
   },
 
   getOwnerHistory: function(serial) {
       var self = this;
-      var history = smartContract.getOwnerHistoryArray.call(serial);
+      var history = smartContract.getOwnerHistoryArray.call(serial, {from: account});
       return history;
   },
 
   deposit: function(amount) {
       var self = this;
-      var dep = smartContract.deposit.sendTransaction({from: EthClient.eth.accounts[0],  gas:1000000, value: amount});
-      var success = smartContract.deposit.call({from: EthClient.eth.accounts[0],  gas:1000000, value: amount});
+      var dep = smartContract.deposit.sendTransaction({from: account,  gas: 1000000, value: amount});
+      var success = smartContract.deposit.call({from: account,  gas: 1000000, value: amount});
       return success;
   },
 
   withdraw: function() {
       var self = this;
-      var dep = smartContract.withdraw.sendTransaction({from: EthClient.eth.accounts[0], gas:1000000});
-      var success = smartContract.withdraw.call({from: EthClient.eth.accounts[0], gas:1000000});
+      var dep = smartContract.withdraw.sendTransaction({from: account, gas:1000000});
+      var success = smartContract.withdraw.call({from: account, gas:1000000});
       return success;
   },
 
   getItemName: function(id) {
     var self = this;
-    var name = smartContract.getItemName.call(id);
+    var name = smartContract.getItemName.call(id, {from: account});
     return name;
   },
 
   getItemsForSale: function() {
     var self = this;
-    var saleArray = smartContract.getItemsForSaleArray.call();
+    var saleArray = smartContract.getItemsForSaleArray.call({from: account});
     return saleArray; //array of uints
   },
 
   getOwnedItems: function() {
     var self = this;
-    var ownedItems = smartContract.getOwnedItemsArray.call();;
+    var ownedItems = smartContract.getOwnedItemsArray.call({from: account});
     return ownedItems; // array of uints
   },
 
   getSalePrice: function(serial) {
     var self = this;
-    var salePrice = smartContract.getSalePrice.call(serial).toNumber();
+    var salePrice = smartContract.getSalePrice.call(serial, {from: account}).toNumber();
     return salePrice;
   },
 
   getIsActive: function(serial) {
     var self = this;
-    var active = smartContract.getIsActive.call(serial);
+    var active = smartContract.getIsActive.call(serial, {from: account});
     return active; // boolean
+
   },
 
   getItemParents: function(serial) {
     var self = this;
-    var parents = smartContract.getItemParents.call(serial);
+    var parents = smartContract.getItemParents.call(serial, {from: account});
     return parents; // array of uints
 
   },
 
   isValidItem: function(serial) {
     var self = this;
-    var valid = smartContract.isValidItem.call(serial);
+    var valid = smartContract.isValidItem.call(serial, {from: account});
     return valid; // boolean
   },
 
   addItem: function(serial, name){
     var self = this;
-    var addCheck = smartContract.addNewItem.call(serial, name);
-    var added = smartContract.addNewItem.sendTransaction(serial, name, {from: EthClient.eth.accounts[0], gas:1000000});
+    var addCheck = smartContract.addNewItem.call(serial, name, {from: account});
+    var added = smartContract.addNewItem.sendTransaction(serial, name, {from: account, gas:1000000});
     return addCheck; // an alert
   },
 
   createNewItem: function(serial, name, parents){
     var self = this;
-    var addCheck = smartContract.createItemWithParents.call(serial, name, parents);
-    var added = smartContract.createItemWithParents.sendTransaction(serial, name, parents, {from: EthClient.eth.accounts[0], gas:1000000});
+    var addCheck = smartContract.createItemWithParents.call(serial, name, parents, {from: account});
+    var added = smartContract.createItemWithParents.sendTransaction(serial, name, parents, {from: account, gas:1000000});
     return addCheck; // an alert
   },
 
   purchaseItem: function(serial){
     var self = this;
-    var pruchaseCheck = smartContract.purchase.call(serial);
-    var purchased = smartContract.purchase.sendTransaction(serial, {from: EthClient.eth.accounts[0], gas:1000000});
+    var pruchaseCheck = smartContract.purchase.call(serial, {from: account});
+    var purchased = smartContract.purchase.sendTransaction(serial, {from: account, gas:1000000});
     return pruchaseCheck; // an alert
   },
 
   markForSale: function(serial, value){
     var self = this;
-    var saleCheck = smartContract.markForSale.call(serial, value);
-    var sale = smartContract.markForSale.sendTransaction(serial, value, {from: EthClient.eth.accounts[0], gas:1000000});
+    var saleCheck = smartContract.markForSale.call(serial, value, {from: account});
+    var sale = smartContract.markForSale.sendTransaction(serial, value, {from: account, gas:1000000});
     return saleCheck; // an alert
   },
 
   undoForSale: function(serial){
     var self = this;
-    var undoCheck = smartContract.undoForSale.call(serial);
-    var undo = smartContract.undoForSale.sendTransaction(serial, {from: EthClient.eth.accounts[0], gas:1000000});
+    var undoCheck = smartContract.undoForSale.call(serial, {from: account});
+    var undo = smartContract.undoForSale.sendTransaction(serial, {from: account, gas:1000000});
     return undoCheck; // an alert
   },
 };
